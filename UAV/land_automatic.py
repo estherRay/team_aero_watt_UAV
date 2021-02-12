17dd11e8b11c1316d3f46d1e02a7b30f3e689a8f
estherRay <77739079+estherRay@users.noreply.github.com>
2021-02-12 20:40:16 +0000

    Update land_automatic.py
    Have only one method to operate appropriate landing. If aircraft exit Glide, just set a boolean that you give to the landing method here.

--- UAV/land_automatic.py
+++ UAV/land_automatic.py
@@ -8,6 +8,4 @@ a special landing mode needs to take place.
 
 # method to determine if aircraft has an acceptable speed and attitude to land.
 
-# method to operate an appropriate landing when exiting glide mode.
-
 # method to operate an appropriate landing.
