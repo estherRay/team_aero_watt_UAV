8dafa419495f89c6bfaa71573e289004a18bd20e
DRGladwell <drg1@hw.ac.uk>
2021-02-12 13:58:59 +0000

    Added a number of key functions used by the UAV

---
+++ UAV/groundmarker_outline_spot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Use low level computer vision techniques to identify potential ground marker locations.
+'''
+
+# method to aquir new camera image and check if blurry
+
+# method to run thresholding on image
+
+# method to check if shape is roughly square and of right size
+
+# method to store in memory aircraft frame, camera frame and potential ground marker. Later the memory storage is
+# used to calculate the positions of all potential ground markers.
+
+# method to check ground marker if close enough (Maybe impliment this
+# )
